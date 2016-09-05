f87654943c4a8d7b59e34b9031c87b89e236539d
Martin Ivanov <martin@mivanov.net>
2016-09-05 19:26:37 +0300

    Remove examples directory

--- kong/resource_kong_consumer_credential_basic_auth.go
+++ kong/resource_kong_consumer_credential_basic_auth.go
@@ -127,7 +127,7 @@ func resourceKongBasicAuthCredentialDelete(d *schema.ResourceData, meta interfac
 	return nil
 }
 
-//TODO: pasword should be SHA1 hashed - https://github.com/Mashape/kong/blob/master/kong/plugins/basic-auth/crypto.lua
+//TODO: pasword should be SHA1 hashed to avoid differences on refresh - https://github.com/Mashape/kong/blob/master/kong/plugins/basic-auth/crypto.lua
 func getBasicAuthCredentialFromResourceData(d *schema.ResourceData) *BasicAuthCredential {
 	basicAuthCredential := &BasicAuthCredential{
 		Username: d.Get("username").(string),
@@ -142,7 +142,7 @@ func getBasicAuthCredentialFromResourceData(d *schema.ResourceData) *BasicAuthCr
 	return basicAuthCredential
 }
 
-//TODO: pasword should be SHA1 hashed - https://github.com/Mashape/kong/blob/master/kong/plugins/basic-auth/crypto.lua
+//TODO: pasword should be SHA1 hashed to avoid differences on refresh - https://github.com/Mashape/kong/blob/master/kong/plugins/basic-auth/crypto.lua
 func setBasicAuthCredentialToResourceData(d *schema.ResourceData, basicAuthCredential *BasicAuthCredential) {
 	d.SetId(basicAuthCredential.ID)
 	d.Set("username", basicAuthCredential.Username)
