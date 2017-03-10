b6617eeb181fc108ddb4bf356dae46eaa4f1bb2b
Hugo Müller-Downing <hugo@localz.co>
2017-03-10 14:01:22 +1100

    Support Kong 0.10.0

--- kong/resource_kong_api.go
+++ kong/resource_kong_api.go
@@ -2,19 +2,29 @@ package kong
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
 	"github.com/dghubble/sling"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
 type API struct {
-	ID               string `json:"id,omitempty"`
-	Name             string `json:"name,omitempty"`
-	Hosts            []interface{} `json:"hosts,omitempty"`
-	RequestPath      string `json:"request_path,omitempty"`
-	StripUri         bool   `json:"strip_uri,omitempty"`
-	PreserveHost     bool   `json:"preserve_host,omitempty"`
-	UpstreamURL      string `json:"upstream_url"`
+	ID                     string        `json:"id,omitempty"`
+	Name                   string        `json:"name,omitempty"`
+	Hosts                  []interface{} `json:"hosts,omitempty"`
+	URIs                   []interface{} `json:"uris"`
+	StripURI               bool          `json:"strip_uri,omitempty"`
+	PreserveHost           bool          `json:"preserve_host,omitempty"`
+	UpstreamURL            string        `json:"upstream_url"`
+	Methods                string        `json:"methods"`
+	Retries                int           `json:"retries"`
+	HTTPSOnly              bool          `json:"https_only"`
+	HTTPIfTerminated       bool          `json:"http_if_terminated"`
+	UpstreamConnectTimeout int           `json:"upstream_connect_timeout,omitempty"`
+	UpstreamSendTimeout    int           `json:"upstream_send_timeout,omitempty"`
+	UpstreamReadTimeout    int           `json:"upstream_read_timeout,omitempty"`
 }
 
 func resourceKongAPI() *schema.Resource {
@@ -34,22 +44,23 @@ func resourceKongAPI() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     nil,
-				Description: "The API name. If none is specified, will default to the request_host or request_path.",
+				Description: "The API name.",
 			},
 
 			"hosts": &schema.Schema{
 				Type:        schema.TypeList,
-				Elem:				 &schema.Schema{Type: schema.TypeString},
+				Elem:        &schema.Schema{Type: schema.TypeString},
 				Optional:    true,
 				Default:     nil,
-				Description: "The public DNS address that points to your API. For example, mockbin.com. At least request_host or request_path or both should be specified.",
+				Description: "A comma-separated list of domain names that point to your API. For example: example.com. At least one of hosts, uris, or methods should be specified.",
 			},
 
-			"request_path": &schema.Schema{
-				Type:        schema.TypeString,
+			"uris": &schema.Schema{
+				Type:        schema.TypeList,
+				Elem:        &schema.Schema{Type: schema.TypeString},
 				Optional:    true,
 				Default:     nil,
-				Description: "The public path that points to your API. For example, /someservice. At least request_host or request_path or both should be specified.",
+				Description: "A comma-separated list of URIs prefixes that point to your API. For example: /my-path. At least one of hosts, uris, or methods should be specified.",
 			},
 
 			"strip_uri": &schema.Schema{
@@ -71,18 +82,72 @@ func resourceKongAPI() *schema.Resource {
 				Required:    true,
 				Description: "The base target URL that points to your API server, this URL will be used for proxying requests. For example, https://mockbin.com.",
 			},
+
+			"methods": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     nil,
+				Description: "A comma-separated list of HTTP methods that point to your API. For example: GET,POST. At least one of hosts, uris, or methods should be specified.",
+			},
+
+			"retries": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     nil,
+				Description: "The number of retries to execute upon failure to proxy. The default is 5.",
+			},
+
+			"https_only": &schema.Schema{
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     nil,
+				Description: "To be enabled if you wish to only serve an API through HTTPS, on the appropriate port (8443 by default). Default: false.",
+			},
+
+			"http_if_terminated": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "Consider the X-Forwarded-Proto header when enforcing HTTPS only traffic. Default: true.",
+			},
+
+			"upstream_connect_timeout": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     nil,
+				Description: "The timeout in milliseconds for establishing a connection to your upstream service. Defaults to 60000.",
+			},
+
+			"upstream_send_timeout": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     nil,
+				Description: "The timeout in milliseconds between two successive write operations for transmitting a request to your upstream service Defaults to 60000.",
+			},
+
+			"upstream_read_timeout": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     nil,
+				Description: "The timeout in milliseconds between two successive read operations for transmitting a request to your upstream service Defaults to 60000.",
+			},
 		},
 	}
 }
 
 func resourceKongAPICreate(d *schema.ResourceData, meta interface{}) error {
 	sling := meta.(*sling.Sling)
-
+	log.Println("RESOURCE KONG API CREATE")
 	api := getAPIFromResourceData(d)
 
 	createdAPI := new(API)
 
-	response, error := sling.New().BodyJSON(api).Post("apis/").ReceiveSuccess(createdAPI)
+	var data interface{} // TopTracks
+	response, error := sling.New().BodyJSON(api).Post("apis/").ReceiveSuccess(data)
+	log.Println("RESPONSE IN RESOURCE KONG API CREATE")
+	log.Print(data)
+	str := spew.Sdump(response)
+	log.Print(str)
+
 	if error != nil {
 		return fmt.Errorf("Error while creating API.")
 	}
@@ -156,12 +221,19 @@ func resourceKongAPIDelete(d *schema.ResourceData, meta interface{}) error {
 
 func getAPIFromResourceData(d *schema.ResourceData) *API {
 	api := &API{
-		Name:             d.Get("name").(string),
-		Hosts:      			d.Get("hosts").([]interface{}),
-		RequestPath:      d.Get("request_path").(string),
-		StripUri:         d.Get("strip_uri").(bool),
-		PreserveHost:     d.Get("preserve_host").(bool),
-		UpstreamURL:      d.Get("upstream_url").(string),
+		Name:         d.Get("name").(string),
+		Hosts:        d.Get("hosts").([]interface{}),
+		URIs:         d.Get("uris").([]interface{}),
+		StripURI:     d.Get("strip_uri").(bool),
+		PreserveHost: d.Get("preserve_host").(bool),
+		UpstreamURL:  d.Get("upstream_url").(string),
+		Methods:      d.Get("methods").(string),
+		Retries:      d.Get("retries").(int),
+		HTTPSOnly:    d.Get("https_only").(bool),
+		// HTTPIfTerminated:       d.Get("http_if_terminated").(bool),
+		UpstreamConnectTimeout: d.Get("upstream_connect_timeout").(int),
+		UpstreamSendTimeout:    d.Get("upstream_send_timeout").(int),
+		UpstreamReadTimeout:    d.Get("upstream_read_timeout").(int),
 	}
 
 	if id, ok := d.GetOk("id"); ok {
@@ -175,8 +247,15 @@ func setAPIToResourceData(d *schema.ResourceData, api *API) {
 	d.SetId(api.ID)
 	d.Set("name", api.Name)
 	d.Set("hosts", api.Hosts)
-	d.Set("request_path", api.RequestPath)
-	d.Set("strip_uri", api.StripUri)
+	d.Set("uris", api.URIs)
+	d.Set("strip_uri", api.StripURI)
 	d.Set("preserve_host", api.PreserveHost)
 	d.Set("upstream_url", api.UpstreamURL)
+	d.Set("methods", api.Methods)
+	d.Set("retries", api.Retries)
+	d.Set("https_only", api.HTTPSOnly)
+	d.Set("http_if_terminated", api.HTTPIfTerminated)
+	d.Set("upstream_connect_timeout", api.UpstreamConnectTimeout)
+	d.Set("upstream_send_timeout", api.UpstreamSendTimeout)
+	d.Set("upstream_read_timeout", api.UpstreamReadTimeout)
 }
