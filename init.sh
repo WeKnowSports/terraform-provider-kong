89cb7defe2b52794d141a1c4f1f350c95b9f0c13
Steve <steven.riley86@gmail.com>
2017-06-26 21:10:22 +1000

    Change library references

--- init.sh
+++ init.sh
@@ -2,9 +2,15 @@
 TF_LOG='DEBUG'
 echo ${1}
 
-rm terraform/init/terraform-provider-kong
-go build -o terraform/init/terraform-provider-kong
+export GOPATH=$HOME/go
+mkdir -p $GOPATH/src/github.com/localz/terraform-provider-kong
+cp -R $(PWD)/ $GOPATH/src/github.com/localz/terraform-provider-kong/
+ls
 
+go build -o terraform/init/terraform-provider-kong
 cd terraform/init
 terraform ${1}
 cd ../..
+
+rm -fr $GOPATH/src/github.com/localz/terraform-provider-kong
+rm -f ./terraform/init/terraform-provider-kong
--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 TF_LOG='DEBUG'
+echo ${1}
 
-rm terraform/tests/terraform-provider-kong
+export GOPATH=$HOME/gocode
+mkdir -p $GOPATH/github.com/localz/terraform-provider-kong
+cp -R $(PWD)/ $GOPATH/src/github.com/localz/terraform-provider-kong/
+
 go build -o terraform/tests/terraform-provider-kong
-
 cd terraform/tests
 terraform ${1}
 cd ../..
+
+rm -fr $GOPATH/github.com/localz/terraform-provider-kong
+rm -f ./terraform/tests/terraform-provider-kong
