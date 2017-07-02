8dd4de3fb8aeff0cafa97287984ddde2c064981c
Martin Ivanov <martin@mivanov.net>
2017-07-02 23:19:33 +0300

    Fix wrong plugin repo

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/martinivanov/terraform-provider-kong/kong"
+	"github.com/WeKnowSports/terraform-provider-kong/kong"
 )
 
 func main() {
