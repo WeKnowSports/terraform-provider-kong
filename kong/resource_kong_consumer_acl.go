48b1c2eb6f472dcf4e3c22307e98b5adc67ba0da
Erran Carey <me@errancarey.com>
2017-10-09 14:58:12 +0100

    Fix import support
    * Implement update which preserves acl id but changes group.
    * `terraform import` can be used with consumer name/id joined with a
      literal "/" and the acl group/id.
    * Fix undetected changes by setting values on Read.

--- kong/resource_kong_consumer_acl.go
+++ kong/resource_kong_consumer_acl.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-type CreateAclRequest struct {
+type UpdateAclRequest struct {
 	Group    string `json:"group"`
 }
 
@@ -22,6 +22,7 @@ func resourceKongConsumerACL() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceKongConsumerACLCreate,
 		Read:   resourceKongConsumerACLRead,
+		Update: resourceKongConsumerACLUpdate,
 		Delete: resourceKongConsumerACLDelete,
 
 		Importer: &schema.ResourceImporter{
@@ -45,7 +46,6 @@ func resourceKongConsumerACL() *schema.Resource {
 			"group": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				ForceNew: true,
 				Description: "The name of the group to place the specified consumer in.",
 			},
 		},
@@ -55,7 +55,7 @@ func resourceKongConsumerACL() *schema.Resource {
 func resourceKongConsumerACLCreate(d *schema.ResourceData, meta interface{}) error {
 	sling := meta.(*sling.Sling)
 
-	createRequest := &CreateAclRequest{
+	createRequest := &UpdateAclRequest{
 		Group: d.Get("group").(string),
 	}
 	consumer := d.Get("consumer").(string)
@@ -80,7 +80,8 @@ func resourceKongConsumerACLRead(d *schema.ResourceData, meta interface{}) error
 	sling := meta.(*sling.Sling)
 
 	consumer := d.Get("consumer").(string)
-	response, error := sling.New().Path("consumers/").Path(consumer).Path("acls/").Get(d.Id()).ReceiveSuccess(nil)
+	updated := &ConsumerACL{}
+	response, error := sling.New().Path("consumers/").Path(consumer + "/").Path("acls/").Get(d.Id()).ReceiveSuccess(updated)
 	if error != nil {
 		return fmt.Errorf("error while reading ACL" + error.Error())
 	}
@@ -92,15 +93,48 @@ func resourceKongConsumerACLRead(d *schema.ResourceData, meta interface{}) error
 		return fmt.Errorf("unexpected status code received: " + response.Status)
 	}
 
+	d.Set("consumer", updated.Consumer)
+	d.Set("group", updated.Group)
+	// Update the id field initally imported via group name.
+	d.SetId(updated.ID)
+
 	return nil
 }
+
+func resourceKongConsumerACLUpdate(d *schema.ResourceData, meta interface{}) error {
+	sling := meta.(*sling.Sling)
+
+	updateRequest := &UpdateAclRequest{
+		Group: d.Get("group").(string),
+	}
+	consumer := d.Get("consumer").(string)
+	updated := &ConsumerACL{}
+	response, error := sling.New().BodyJSON(updateRequest).Path("consumers/").Path(consumer + "/").Path("acls/").Patch(d.Id()).ReceiveSuccess(updated)
+	if error != nil {
+		return fmt.Errorf("error while creating ACL" + error.Error())
+	}
+
+	if response.StatusCode == http.StatusNotFound {
+		d.SetId("")
+		return nil
+	} else if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code received: " + response.Status)
+	}
+
+	d.Set("consumer", updated.Consumer)
+	d.Set("group", updated.Group)
+	d.SetId(updated.ID)
+
+	return nil
+}
+
 
 func resourceKongConsumerACLDelete(d *schema.ResourceData, meta interface{}) error {
 	sling := meta.(*sling.Sling)
 
-	id := d.Get("id").(string)
 	consumer := d.Get("consumer").(string)
-	response, error := sling.New().Delete("consumers/").Path(consumer).Path("acls/").Path(id).ReceiveSuccess(nil)
+	response, error := sling.New().Path("consumers/").Path(consumer + "/").Path("acls/").Delete(d.Id()).ReceiveSuccess(nil)
+
 	if error != nil {
 		return fmt.Errorf("error while deleting ACL" + error.Error())
 	}
