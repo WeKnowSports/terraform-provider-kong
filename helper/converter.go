821fca94485abed40a0ee626133dcab262e84229
Yevhenii Semenko <y.semenko@draftkings.com>
2023-06-22 17:49:46 +0200

    add option to enable the plugin on service, route, or consumer simultaneously

--- helper/converter.go
+++ helper/converter.go
@@ -7,3 +7,28 @@ func ConvertInterfaceArrToStrings(strs []interface{}) []string {
 	}
 	return arr
 }
+
+func SetObjectID(ID string) map[string]string {
+	results := map[string]string{}
+
+	if ID == "" {
+		return nil
+	}
+
+	results["id"] = ID
+	return results
+}
+
+func SetConsumerID(ID, username string) map[string]string {
+	results := map[string]string{}
+
+	if ID == "" && username == "" {
+		return nil
+	} else if ID != "" {
+		results["id"] = ID
+	} else if username != "" {
+		results["username"] = username
+	}
+
+	return results
+}
